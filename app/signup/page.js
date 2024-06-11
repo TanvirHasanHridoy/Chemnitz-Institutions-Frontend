784d7d7170624c7b64a6e3f5f42d03f226e374cb
HRIDOY <th.hridoy2013@gmail.com>
2024-06-11 23:29:26 +0200

    Added user from client side

--- app/signup/page.js
+++ app/signup/page.js
@@ -2,7 +2,6 @@
 import Head from "next/head";
 import { useState, useRef } from "react";
 import { useLoadScript, Autocomplete } from "@react-google-maps/api";
-
 const libraries = ["places"];
 const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
@@ -13,9 +12,18 @@ export default function SignUp() {
     googleMapsApiKey,
     libraries,
   });
-
+  // form variables
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [address, setAddress] = useState("");
+  const [lat, setLat] = useState("");
+  const [lan, setLan] = useState("");
+
   const [isAddressSelected, setIsAddressSelected] = useState(false);
+
+  // refs
+  const formRef = useRef(null);
   const autocompleteRef = useRef(null);
 
   const handleSelect = (address) => {
@@ -30,15 +38,72 @@ export default function SignUp() {
     );
     console.log("Latitude:", lat());
     console.log("Longitude:", lng());
+    setLat(lat());
+    setLan(lng());
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (!isAddressSelected) {
       alert("Please select an address from the suggestions.");
       return;
     }
+    const formData = {
+      name: name,
+      email: email,
+      password: password,
+      address: address,
+      lat: lat,
+      lan: lan,
+    };
+    console.log(formData);
     // Add your form submission logic here
+    // Add your form submission logic here, for example:
+    // try {
+    //   const response = await fetch("http://localhost:3000/user/createuser", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(formData),
+    //   }).then((res) => res.json());
+    //   if (response.ok) {
+    //     console.log("Success:", response.json());
+    //     alert("User created successfully");
+    //     console.log(response);
+    //   } else {
+    //     alert(response.msg);
+    //     // if (response.status === 400) throw new Error("400, Very bad request");
+    //   }
+    // } catch (error) {
+    //   console.error("Error while fetching:", error);
+    //   alert("");
+    // }
+    fetch("http://localhost:3000/user/createuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Success:", response.json());
+          alert("User created successfully");
+          console.log(response);
+          window.history.pushState({}, "", "/signin");
+          window.dispatchEvent(new Event("popstate"));
+        } else {
+          response.json().then((data) => {
+            console.log("Error:", data);
+            alert(data.msg);
+          });
+        }
+      })
+      .catch((error) => {
+        alert(error.msg);
+        console.error("Error:", error);
+      });
   };
 
   if (loadError) {
@@ -64,13 +129,15 @@ export default function SignUp() {
       </Head>
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
-        <form className="space-y-4" onSubmit={handleSubmit}>
+        <form ref={formRef} className="space-y-4" onSubmit={handleSubmit}>
           <div>
-            <label className="block text-gray-700">Username</label>
+            <label className="block text-gray-700">Name</label>
             <input
               type="text"
               required
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+              onChange={(e) => setName(e.target.value)}
+              autoComplete="name"
             />
           </div>
           <div>
@@ -79,6 +146,7 @@ export default function SignUp() {
               type="email"
               required
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div>
@@ -87,6 +155,7 @@ export default function SignUp() {
               type="password"
               required
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div>
